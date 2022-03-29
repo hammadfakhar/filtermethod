54a44f82f48d3745d26eed625a0fca124865cc12
hammadfakhar <hammadfakhar51@gmail.com>
2022-03-29 17:56:25 +0500

    work complete

---
+++ index.js
@@ -0,0 +1,39 @@
+// let a = [1,2,3,4,5,6,7]
+// let b = a.filter(function(x){
+// if(x < 6){
+//     return x
+// }
+//     })
+// console.log(b)
+
+
+var obj1 = [
+    {
+    name: 'hammad',
+    id: 1,
+    contact : 0300,
+    
+},
+{
+    name: 'hassan',
+    id: 2,
+    contact: 0100
+},
+{
+    name : 'subhan',
+    id:   3,
+    contact: 0200
+}
+]
+
+let filteredItems = obj1.map(function(x){
+
+    return x
+      
+    })
+
+ 
+// console.log(filteredItems)
+var arr = []
+arr.push(filteredItems)
+console.log(arr)
